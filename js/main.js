29b584b35575245526c1dad3b49a5ce7b515dc55
WhangInWha <caleb123@daum.net>
2021-10-01 18:30:15 +0900

    1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert(1);
+$(document).ready(function(){
+    $('body').on('submit', '#searchForm', function (e) {
+        let searchText = $('#searchText');
+        alert(searchText);
+        e.preventDefault();
+    });
+});
