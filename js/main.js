5cd9f3b4e904240a05e198a95b34deec0020ccd7
WhangInWha <caleb123@daum.net>
2021-10-01 18:44:48 +0900

    4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('body').on('submit', '#searchForm', function (e) {
-        let searchText = $('#searchText');
-        alert(searchText);
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
         e.preventDefault();
     });
 });
+
+//Get Movies From OMDB API
+function getMovies(searchText) {
+    $.ajax({
+        method: 'Get',
+        url: 'http://www.omdbapi.com?s=' + searchText
+    }).done(function (data) {
+        console.log(data);
+    });
+}
